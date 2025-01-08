d6983b701232c921befab1a99ea1973ff6ec65c6
melkyv <melkytam17@gmail.com>
2025-01-08 19:22:12 -0300

    Adiciona classe para tratamento de erros

---
+++ src/main/java/com/mkv/devcatalog/infra/exception/ErrorHandler.java
@@ -0,0 +1,15 @@
+package com.mkv.devcatalog.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity notFound() {
+        return ResponseEntity.status(404).body("Register not found");
+    }
+}
