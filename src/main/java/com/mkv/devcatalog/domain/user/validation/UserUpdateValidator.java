5e05f4faa64b7a3a86dce2d6e5f836bf6d643e70
melkyv <melkytam17@gmail.com>
2025-02-03 22:19:47 -0300

    Atualiza validação de update de usuário

--- src/main/java/com/mkv/devcatalog/domain/user/validation/UserUpdateValidator.java
+++ src/main/java/com/mkv/devcatalog/domain/user/validation/UserUpdateValidator.java
@@ -4,14 +4,20 @@ import com.mkv.devcatalog.domain.user.User;
 import com.mkv.devcatalog.domain.user.UserRepository;
 import com.mkv.devcatalog.domain.user.UserUpdateDTO;
 import com.mkv.devcatalog.infra.exception.ValidationErrorData;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private UserRepository repository;
@@ -23,10 +29,14 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
     @Override
     public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
 
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
+
         List<ValidationErrorData> list = new ArrayList<>();
 
         User user = repository.findByEmail(dto.getEmail());
-        if (user != null) {
+        if (user != null && userId != user.getId()) {
             list.add(new ValidationErrorData("email", "Email já existe"));
         }
 
